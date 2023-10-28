dde0e9034615884a9ffd8ee1fc3bd049abd4022d
Rafel Ballester Coll <rballestercoll@gmail.com>
2023-10-28 19:13:45 +0200

    Versión final de la primera entrega

--- Front_end/JS/asignatura.js
+++ Front_end/JS/asignatura.js
@@ -1,56 +1,53 @@
-// JSON data for subjects
+// Datos JSON de las Cards
 var asignaturasData = {
   "23241": {
-    numSemester: 1,
-    year: "23/24",
+    name: "Full Stack JS",
     dateStart: "",
     dateEnd: "",
     color: "#33aaff",
     description: "El semestre más importante",
     opinion: "Me encanta",
     difficulty: 1,
-    status: "asignaturas-pending",
+    status: "asignaturas-pending"
   },
   "23242": {
-    numSemester: 2,
-    year: "23/24",
+    name: "Competencias Digitales",
     dateStart: "",
     dateEnd: "",
     color: "#CCCCCC",
     description: "El semestre más importante",
     opinion: "Me encanta",
     difficulty: 3,
-    status: "empezada",
-  },
-  // Add more subjects as needed
+    status: "empezada"
+  }
 };
 
-// Function to draw the subject cards
+// Función para extraer las tarjetas temáticas
 function drawAsignaturas(asignaturas) {
-  // Get the container where cards will be added
-  var container = $('#asignaturas-pending');
+  // Obtener el contenedor donde se añadirán las tarjetas
+  var container = $("#asignaturas-pending");
 
-  // Clear the container
+  // Vaciar el contenedor
   container.empty();
 
-  // Iterate through the subjects and create cards
+  // Iterar a través de los temas y crear tarjetas
   $.each(asignaturas, function (index, asignatura) {
     var card = createCard(index, asignatura);
     container.append(card);
   });
 
-  // Enable drag and drop functionality
+  // Activar la función de arrastrar y soltar
   dragAndDrop();
 }
 
-// Function to create a single subject card
+// Función para crear una tarjeta temática única
 function createCard(index, asignatura) {
   var card = `
     <div class="margen-top-lg col-sm-4 ${asignatura.status}" id="${index}">
       <div class="card" style="width: 18rem;">
         <div class="card-body">
           <h5 class="card-title" style="background-color: ${asignatura.color};">
-            Asignatura ${asignatura.description} del curso ${asignatura.year}
+            Asignatura: ${asignatura.name}
           </h5>
           <div class="padding-sm">
             <p class="card-text">
@@ -60,8 +57,7 @@ function createCard(index, asignatura) {
               Opinión: ${asignatura.opinion}<br>
               Dificultad: ${asignatura.difficulty}
             </p>
-            <a href="#" class="btn btn-primary" id="añadir" onclick="drawModalAsignatura(${index})">Editar</a>
-            <a href="interfaz_3.html" class="btn btn-primary" id="añadir" onclick="drawModalAsignatura(${index})">Ver</a>
+            <a href="#" class="btn btn-primary" id="editarAsignatura" onclick="drawModalAsignatura(${index})">Editar</a>
             <button class="btn btn-danger" id="eliminar" onclick="eliminar(${index})"><i class="fas fa-trash"></i></button>
           </div>
         </div>
@@ -72,73 +68,82 @@ function createCard(index, asignatura) {
   return card;
 }
 
-// Function to enable drag and drop functionality
+// Función para activar la función de arrastrar y soltar
 function dragAndDrop() {
   $(".draggable").draggable({
-    helper: "clone",
+    helper: "clone"
   });
 
   $(".column").sortable({
-    connectWith: ".column",
+    connectWith: ".column"
   });
 }
 
-// Function to open the modal for a new subject
+// Función para abrir el modal de un nuevo asunto
 function drawModalAsignatura() {
-  // Reset the form
-  $('#asignaturaForm')[0].reset();
-  
-  // Show the modal
-  $('#modalAsignatura').modal('show');
+  // Restablecer el formulario
+  $("#asignaturaForm")[0].reset();
+
+  // Mostrar el modal
+  $("#modalAsignatura").modal("show");
 }
 
-// Handle click on "Añadir Asignatura" button
-$('#añadir-asignatura').click(function () {
+// Haga clic en el botón "Añadir Asignatura".
+$("#añadir-asignatura").click(function () {
   drawModalAsignatura();
 });
+  // Si viene definido un índice de asignatura, cargamos los datos en el formulario
+$("#editar-asignatura").click(function (index) {
+  if (index && typeof asignaturasData[index] != "undefined") {
+    asignatura = asignaturasData[index];
+    title = "Ver/Editar ";
+  }
+  drawModalAsignatura();
+  
+});
+// Haga clic en el botón "Guardar" en el modal
+$("#guardarAsignatura").click(function () {
+  // Obtener datos del formulario
+  var nombreAsignatura = $("#nombreAsignatura").val();
+  var fechaInicio = $("#fechaInicio").val();
+  var fechaFin = $("#fechaFin").val();
+  var descripcion = $("#descripcion").val();
+  var opinion = $("#opinion").val();
+  var color = $("#color").val();
+  var dificultad = $("#dificultad").val();
 
-// Handle click on "Guardar" button in the modal
-$('#guardarAsignatura').click(function () {
-  // Get data from the form
-  var asignatura = $('#asignatura').val();
-  var fechaInicio = $('#fechaInicio').val();
-  var fechaFin = $('#fechaFin').val();
-  var descripcion = $('#descripcion').val();
-  var opinion = $('#opinion').val();
-  var color = $('#color').val();
-  var dificultad = $('#dificultad').val();
-
-  // Create a new subject object
+  // Crear un nuevo objeto sujeto
   var newSubject = {
-    numSemester: 1,
-    year: "23/24",
+    name: nombreAsignatura,
     dateStart: fechaInicio,
     dateEnd: fechaFin,
     color: color,
     description: descripcion,
     opinion: opinion,
     difficulty: dificultad,
-    status: "asignaturas-pending",
+    status: "asignaturas-pending"
   };
 
-  // Add the new subject to the data
+  // Añadir el nuevo sujeto a los datos
   var newIndex = Date.now().toString();
   asignaturasData[newIndex] = newSubject;
 
-  // Create and append a new card
+  // Crear y añadir una nueva tarjeta
   var newCard = createCard(newIndex, newSubject);
-  $('#asignaturas-pending').append(newCard);
+  $("#asignaturas-pending").append(newCard);
 
-  // Hide the modal
-  $('#modalAsignatura').modal('hide');
+  // Ocultar el modal
+  $("#modalAsignatura").modal("hide");
 });
 
-// Document ready function
+// Función de documento listo
 $(document).ready(function () {
   drawAsignaturas(asignaturasData);
 });
 
+/*
 // Handle changes in the select element with id "select-semana"
 $("#select-semana").change(function () {
   // Update subjects based on the selected week if needed
 });
+*/
